7072eea9cc046d845cb27e39b4fc9973f3141ad1
takhmina0907 <lollis0907@gmail.com>
2018-07-29 20:03:18 +0600

    Registration UI and Firebase

--- Eco/Controller/MyPageViewController.swift
+++ Eco/Controller/MyPageViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class MyPageViewController: UIPageViewController, UIPageViewControllerDataSource {
+class MyPageViewController: UIPageViewController, UIPageViewControllerDataSource, UIPageViewControllerDelegate {
 
     lazy var viewControllerList:[UIViewController] = {
         let sb = UIStoryboard(name: "Main", bundle: nil)
@@ -20,14 +20,33 @@ class MyPageViewController: UIPageViewController, UIPageViewControllerDataSource
         return [vc1, vc2, vc3]
     }()
     
+    var pageControl = UIPageControl()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.dataSource = self
+        self.delegate = self
+        configurePageControl()
         
         if let firstViewController = viewControllerList.first{
             self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
+    }
+    //Dots
+    func configurePageControl() {
+        // The total number of pages that are available is based on how many available colors we have.
+        pageControl = UIPageControl(frame: CGRect(x: 0,y: UIScreen.main.bounds.maxY - 50,width: UIScreen.main.bounds.width,height: 50))
+        self.pageControl.numberOfPages = viewControllerList.count
+        self.pageControl.currentPage = 0
+        self.pageControl.tintColor = UIColor.black
+        self.pageControl.pageIndicatorTintColor = UIColor.lightGray
+        self.pageControl.currentPageIndicatorTintColor = UIColor.black
+        self.view.addSubview(pageControl)
+    }
+    func pageViewController(_ pageViewController: UIPageViewController, didFinishAnimating finished: Bool, previousViewControllers: [UIViewController], transitionCompleted completed: Bool) {
+        let pageContentViewController = pageViewController.viewControllers![0]
+        self.pageControl.currentPage = viewControllerList.index(of: pageContentViewController)!
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
